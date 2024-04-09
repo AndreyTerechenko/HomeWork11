07bdcc58e7f6879fb3be9319b5d1b82f9e6cde59
AndreyTerechenko <Dronka97@yandex.ru>
2024-04-09 21:48:56 +0300

    error corection

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
-
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         //Task1
         int year = 2021;
-        if (countingYears(year)) {
+        if (isLeapYear(year)) {
             System.out.println(year + " год является високосным");
         } else System.out.println(year + " год не является високосным");
         // Task2
-        examinationOs(1, 2024);
+        examinationOs(1, 2023);
         examinationOs(0, 2019);
         //Task3
-        System.out.println("Доставка займет " + countingDays(30) + " дней");
+        int deliveryDistance = 30;
+        if (countingDays(deliveryDistance) >= 0) {
+            System.out.println("Доставка займет " + countingDays(deliveryDistance) + " дней");
+        } else System.out.println("Доставка не осуществляется");
     }
 
     public static void examinationOs(int clientOs, int clientDeviceYear) {
@@ -20,15 +22,15 @@ public class Main {
         if (clientOs == 0 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения для Ios по ссылке");
         } else if (clientOs == 0 && clientDeviceYear == currentYear) {
-            System.out.println("становите версию приложения для iOS по ссылке");
+            System.out.println("Установите версию приложения для iOS по ссылке");
         } else if (clientOs == 1 && clientDeviceYear < currentYear) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (clientOs == 1 && clientDeviceYear == currentYear) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        }
+        } else System.out.println("Год введен не верно");
     }
 
-    public static boolean countingYears(int year) {
+    public static boolean isLeapYear(int year) {
 
         return (year >= 1584 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0));
     }
@@ -40,7 +42,6 @@ public class Main {
             return 2;
         } else if (deliveryDistance < 100) {
             return 3;
-        } else System.out.println("Доставка не осуществляется");
-        return -1;
+        } else return -1;
     }
 }
